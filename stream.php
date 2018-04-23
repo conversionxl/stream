d5489ae3be4b5d49b7ce6cdd2704cd9e1a937565
leho <leho@conversionready.com>
2018-04-23 10:39:37 +0300

    version git

--- stream.php
+++ stream.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Stream
+ * Plugin Name: Stream (git)
  * Plugin URI: https://xwp.co/work/stream/
  * Description: Stream tracks logged-in user activity so you can monitor every change made on your WordPress site in beautifully organized detail. All activity is organized by context, action and IP address for easy filtering. Developers can extend Stream with custom connectors to log any kind of action.
  * Version: 4.0.0
